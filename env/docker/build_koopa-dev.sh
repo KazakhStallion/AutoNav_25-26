001ffd591bbba22369b36a629360497953aa3814
Tamir <tamirlan@gmail.com>
2025-10-29 20:42:42 -0400

    Revert "Revert the changes to docker build and run files"
    This reverts commit dae4f90d11f9cd619f87648152a03b1e057c4b00.

--- env/docker/build_koopa-dev.sh
+++ env/docker/build_koopa-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="dev"
+IMAGE_NAME="autonav"
 IMAGE_TAG="koopa-kingdom"
 
 ROOT="$(dirname ${BASH_SOURCE[0]})"
@@ -13,3 +13,6 @@ fi
 
 
 docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" "$HOME/AutoNav_25-26/env/docker/dockerfiles" 
+
+
+
