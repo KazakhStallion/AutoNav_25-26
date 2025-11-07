da7528f0fa637c09fe0cf1d9af969407fd5a53b4
Tamir <tamirlan@gmail.com>
2025-11-07 17:30:51 -0500

    Made changes to the customize.sh

--- env/docker/entrypoint_additions/customize.sh
+++ env/docker/entrypoint_additions/customize.sh
@@ -2,7 +2,9 @@
 
 BASHRC="/home/admin/.bashrc"
 
-echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]bowser\[\033[01;33m\]@\[\033[01;32m\]koopa-kingdom\[\033[00m\]:\[\033[01;33m\]\w\[\033[00m\]\$ '" >> $BASHRC
-echo "source /opt/ros/humble/setup.bash" >> $BASHRC
-echo "source /autonav/isaac_ros-dev/install/setup.bash" >> $BASHRC
+add_once() { grep -qxF "$1" "$BASHRC" || echo "$1" >> "$BASHRC"; }
+
+add_once "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]bowser\[\033[01;33m\]@\[\033[01;32m\]koopa-kingdom\[\033[00m\]:\[\033[01;33m\]\w\[\033[00m\]\$ '"
+add_once "[ -f source /opt/ros/humble/setup.bash] && source /opt/ros/humble/setup.bash"
+add_once "[ -f source /autonav/isaac_ros-dev/install/setup.bash ] && source /autonav/isaac_ros-dev/install/setup.bash"
 
