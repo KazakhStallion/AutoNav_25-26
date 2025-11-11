792adf6647dbf971f558206efbb24ffc81ed56f2
Tamir <tamirlan@gmail.com>
2025-11-11 15:08:53 -0500

    Update sick_args{}

--- isaac_ros-dev/src/bringup/launch/sensors.launch.py
+++ isaac_ros-dev/src/bringup/launch/sensors.launch.py
@@ -43,6 +43,8 @@ def generate_launch_description():
     sick_args = {
         'hostname':'192.168.0.1',
         'udp_receiver_ip':'192.168.0.2',
+        'hostname': LaunchConfiguration('hostname'),
+        'udp_receiver_ip': LaunchConfiguration('udp_receiver_ip'),
         'publish_frame_id':'lidar_footprint',
         'tf_publish_rate':'0'
     }
